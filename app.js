afef2a0c5cad9565c0c42b7212f19d7713e4a780
Diego Avila <diegoezequielavila95@gmail.com>
2025-02-19 21:33:56 -0300

    creando-la-funcion

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
 // Array para almacenar los nombres de los amigos
 
 let amigos = [];
+
+// Función para agregar un amigo
+function agregarAmigo() {
+    const inputAmigo = document.getElementById('amigo');   // Obtiene el valor del input
+    const nombreAmigo = inputAmigo.value.trim(); // Elimina los espacios en blanco al inicio y al final
+// Validando que el input no este vacío
+    if (nombreAmigo === '') {
+        alert('Debes ingresar un nombre');
+        return;
+    }
+}
